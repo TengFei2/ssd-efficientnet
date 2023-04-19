181e419bd6c3f232ee9b356530a76769d393ce6a
FEI <98009854+TengFei2@users.noreply.github.com>
2023-04-19 14:10:47 +0800

    Update my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -12,23 +12,30 @@ class YOLODataset(data.Dataset):
     读取YOLO 数据格式的数据集
     数据标注：1->circle, 2->Yes, 3->No
     '''
-    def __init__(self, yolo_root, transforms=None, is_train=True): # 数据集文件夹camera2/images 包含训练集合和测试集
+    def __init__(self, root, transforms=None, is_train=True): # 数据集文件夹camera2/images 包含训练集合和测试集
         super().__init__()
         if is_train:
-            self.data_root = Path(yolo_root) / 'images/train'
-            self.lables_root = Path(yolo_root) / 'labels/train'
-            self.dataname = [i.stem for i in self.data_root.glob('*.jpg')]
+            # root = '/kaggle/input/diordata/'
+            self.img_path = Path(root) / 'images'
+            self.lable_path = Path(root) / 'labels'
+            with open(str(Path(root) / 'ImageSets/train.txt'), 'r') as f:
+                x = f.readlines()
+            self.dataname = [i.strip() for i in x]
+
+
         else:
-            self.data_root = Path(yolo_root) / 'images/valid'
-            self.lables_root = Path(yolo_root) / 'labels/valid'
-            self.dataname = [i.stem for i in self.data_root.glob('*.jpg')]
+            self.img_path = Path(root) / 'images'
+            self.lable_path = Path(root) / 'labels'
+            with open(str(Path(root) / 'ImageSets/val.txt'), 'r') as f:
+                x = f.readlines()
+            self.dataname = [i.strip() for i in x]
         self.transforms = transforms
 
     def __len__(self):
         return len(self.dataname)
     
     def __getitem__(self, index):
-        image = Image.open(str(self.data_root / (self.dataname[index] + '.jpg')))
+        image = Image.open(str(self.img_path / (self.dataname[index] + '.jpg')))
         data_width, data_height = image.size
         height_width = [data_height, data_width]
 
@@ -36,7 +43,7 @@ class YOLODataset(data.Dataset):
 
         # read txt
         boxes, labels = [], []
-        with open(str(self.lables_root / (self.dataname[index] + '.txt')), 'r') as f:
+        with open(str(self.lable_path / (self.dataname[index] + '.txt')), 'r') as f:
             for line in f.readlines():
                 x = [float(i) for i in line.split(' ')]
                 boxes.append([x[1:]])
@@ -77,14 +84,14 @@ class YOLODataset(data.Dataset):
         Args:
             idx: 输入需要获取图像的索引
         """
-        image = Image.open(str(self.data_root / (self.dataname[index] + '.jpg')))
+        image = Image.open(str(self.img_path / (self.dataname[index] + '.jpg')))
         data_width, data_height = image.size
         height_width = [data_height, data_width]
 
 
         # read txt
         boxes, labels = [], []
-        with open(str(self.lables_root / (self.dataname[index] + '.txt')), 'r') as f:
+        with open(str(self.lable_path / (self.dataname[index] + '.txt')), 'r') as f:
             for line in f.readlines():
                 x = [float(i) for i in line.split(' ')]
                 boxes.append([x[1:]])
@@ -137,23 +144,24 @@ class YOLODataset(data.Dataset):
     
 
 if __name__ == '__main__':
-    import transforms
-    os.system('clear')
-    data_transform = {
-        'train':transforms.Compose([transforms.SSDCropping(),
-                                    transforms.Resize(),
-                                    transforms.ColorJitter(),
-                                    transforms.ToTensor(),
-                                    transforms.RandomHorizontalFlip(),
-                                    transforms.Normalization(),
-                                    transforms.AssignGTtoDefaultBox()]),
-        'val':transforms.Compose([transforms.Resize(),
-                                  transforms.ToTensor(),
-                                  transforms.Normalization()])}
-    x = YOLODataset('../camera', transforms=data_transform['train'])
-    for image, target in x:
-        # image
-        print(target)
-        print(target['labels'].shape)
-        print(target['labels'].max())
-        break
+    # import transforms
+    # os.system('clear')
+    # data_transform = {
+    #     'train':transforms.Compose([transforms.SSDCropping(),
+    #                                 transforms.Resize(),
+    #                                 transforms.ColorJitter(),
+    #                                 transforms.ToTensor(),
+    #                                 transforms.RandomHorizontalFlip(),
+    #                                 transforms.Normalization(),
+    #                                 transforms.AssignGTtoDefaultBox()]),
+    #     'val':transforms.Compose([transforms.Resize(),
+    #                               transforms.ToTensor(),
+    #                               transforms.Normalization()])}
+    # x = YOLODataset('../camera', transforms=data_transform['train'])
+    # for image, target in x:
+    #     # image
+    #     print(target)
+    #     print(target['labels'].shape)
+    #     print(target['labels'].max())
+    #     break
+    x = YOLODataset('dior')
